b4e41e8aec0b1545c826d77c61309fb34b201bcf
cetin hantas <cetinhantas@gmail.com>
2025-07-18 17:44:30 +0300

    updated code

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,45 @@
 /*   By: chantas <chantas@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 18:45:53 by chantas           #+#    #+#             */
-/*   Updated: 2025/07/17 03:05:26 by chantas          ###   ########.fr       */
+/*   Updated: 2025/07/18 17:40:59 by chantas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*next_line(char **buffer, char *temp, char **line, int fd)
+static char	*next_line(char **buffer, char *temp, char *line, int fd)
 {
-	int		i;
-	int		bytes;
-
 	read(fd, temp, BUFFER_SIZE);
-	i = ft_strchr(temp, '\n');
-	while (!i)
+	while (!ft_strchr(temp, '\n'))
 	{
-		buffer = ft_strdup(temp);
-		bytes = read(fd, temp, BUFFER_SIZE);
-		if (bytes)
-			buffer = ft_strjoin(buffer, temp);
+		*buffer = ft_strjoin(*buffer, temp);
+		if (read(fd, temp, BUFFER_SIZE))
+			*buffer = ft_strjoin(*buffer, temp);
 		else
 		{
-			i = ft_strlen(buffer);
-			break ;
+			line = ft_substr(*buffer, ft_strlen(*buffer));
+			return (free(*buffer), free(temp), line);
 		}
-		i = ft_strchr(buffer, '\n');
 	}
-	if (i)
-		*line = ft_substr(buffer, i);
+	if (!**buffer)
+		line = ft_substr(temp, ft_strchr(temp, '\n'));
+	else
+		line = ft_substr(*buffer, ft_strchr(*buffer, '\n'));
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	static char	*line;
-	static char	*temp;
+	char		*line;
+	char		*temp;
 
 	temp = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		buffer = ft_strdup("");
 	if (fd < 0 || !temp || !buffer || read(fd, 0, 0) <= 0)
-		return (free(buffer), NULL);
+		return (free(buffer), free(buffer), NULL);
 	temp[BUFFER_SIZE] = 0;
-	line = next_line(&buffer, temp, &line, fd);
+	line = next_line(&buffer, temp, line, fd);
 	return (line);
 }
